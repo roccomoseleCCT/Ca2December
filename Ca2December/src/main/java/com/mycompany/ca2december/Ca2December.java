f164278154be7d71519be59e2b3b90f1812da88a
roccomoseleCCT <sba24436@student.cct.ie>
2024-12-23 11:27:58 +0100

    Creation of the Java Application
    Creation of the Java Application

---
+++ Ca2December/src/main/java/com/mycompany/ca2december/Ca2December.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ca2december;
+
+/**
+ *
+ * @author agnes
+ */
+public class Ca2December {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
